05c3aab1664d4883b6a02f2e7f0c3b104c4e6a24
ankpan18 <panthriankur@outlook.com>
2022-07-09 12:09:49 +0530

    Adding files first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ButtonPanel from './components/ButtonPanel';
+import Display from './components/Display';
+import calculate from './logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      total:null,
+      next: null,
+      operation:null,
+    };
+  }
+  handleClick=(buttonName)=>{
+    this.setState(calculate(this.state,buttonName));
+  }
+  render() {
+    return (
+      <div className="component-app">
+        <Display  value={this.state.next || this.state.total || '0'}
+        />
+        <ButtonPanel clickHandler={this.handleClick}/>
+      </div>
+    );  
+  }
 }
-
 export default App;
